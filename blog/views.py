ab238412a921806ee596414839f5a377ecb1125f
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-05-01 19:45:08 +0200

    add review

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render,redirect
+from .models import Post,Review
+from .forms import ReviewForm
 
 def post_list(request):
 
@@ -13,9 +14,22 @@ def post_list(request):
 
 def post_detail(request,slug):
     post=Post.objects.get(slug=slug)
+    reviews=Review.objects.filter(post=post)
+
+    if request.method =='POST':
+        form=ReviewForm(request.POST)
+        if form.is_valid():
+            myform=form.save(commit=False)
+            myform.post=post
+            form.save()
+            return redirect(f'/blog/{post.slug}')
+    else:
+        form=ReviewForm()
 
     context={
-        'post':post
+        'post':post,
+        'reviews':reviews,
+        'form':form,
     }
 
 
