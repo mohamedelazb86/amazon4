e491128c532b8d844b7479b4812e57885d7e9ac4
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-05-01 17:25:02 +0200

    send gamil

--- project/settings.py
+++ project/settings.py
@@ -134,8 +134,19 @@ MEDIA_ROOT= BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 AUTHENTICATION_BACKENDS=[
     'accounts.backends.Login_Email_Username'
 ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ('mohamedelazab2017@gmail.com')
+EMAIL_HOST_PASSWORD = ('srkwhtbnesinqxqo')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
