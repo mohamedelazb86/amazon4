168f79d6c41f1349cc68d9c29015c8607f07c354
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-05-01 11:20:40 +0200

    create profile model

---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from utils.generate_code import generate_code
+
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,related_name='profile_user',on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='photo_user')
+    code=models.CharField(max_length=50,default=generate_code)
+
+
+    def __Str__(self):
+        return str(self.user)
