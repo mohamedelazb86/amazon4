2bf3d4cdd57787b423bc41d363583e4ac7e56302
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-05-04 14:59:13 +0200

    add tubliner

--- products/admin.py
+++ products/admin.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Product,Review,ImagePOroduct,Brand
 
+class ImgaeAmin(admin.TabularInline):
+    model=ImagePOroduct
+
 class ProductAdmin(SummernoteModelAdmin):
     list_display=['name','sku','flag']
     list_filter=['brand']
     search_fields=['name','subtitle','descriptions']
     summernote_fields=('subtitle','descriptions')
+    inlines=[ImgaeAmin]
 
 
 
