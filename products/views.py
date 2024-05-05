7175caaafda862cac6868bd766acb280cf1fd7c0
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-05-05 09:22:39 +0200

    add brand detail first

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product,Brand,Review,ImagePOroduct
 from django.views.generic import ListView,DetailView
+from django.db.models.aggregates import Count
 
 
 class Product_List(ListView):
@@ -25,7 +26,14 @@ class Brand_list(ListView):
     model=Brand
     template_name='products/brand_list.html'
     paginate_by=25
+    queryset=Brand.objects.annotate(product_count=Count('product_brand'))
 
 class Brand_Detail(DetailView):
     model=Brand
     template_name='products/brand_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["related"] = Product.objects.filter(brand=self.get_object())
+        return context
+    
