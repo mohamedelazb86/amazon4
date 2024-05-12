da8e42fc6733c1e67a53502c2dc73ae7699ae7f8
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-05-12 12:11:31 +0200

    add create order api and get_update_delete cart

--- orders/api.py
+++ orders/api.py
@@ -5,6 +5,8 @@ from rest_framework import status
 import datetime
 from .models import Cart,Cart_Detail,Order,Order_Detail,Copoun
 from settings.models import Deliver_Fee
+from products.models import Product
+from accounts.models import Address
 from django.shortcuts import get_object_or_404
 from .serializers import CartDetailSerializers,CartSerializers,OrderDetailSerializers,OrderSerializers
 
@@ -55,4 +57,82 @@ class Apply_Copoun(generics.GenericAPIView):
             else:
                 return Response ({'message':'sorry copoun expired'},status=status.HTTP_401_UNAUTHORIZED)
         return Response ({'message':'sorry this copoun not found pls try agin'},status=status.HTTP_404_NOT_FOUND)
-                
+    
+
+class Create_Order(generics.GenericAPIView):
+    def post(self,request,*args,**kwargs):
+        user=User.objects.get(username=self.kwargs['username'])
+        cart=Cart.objects.get(user=user,status='Inprogress')
+        cart_detail=Cart_Detail.objects.filter(cart=cart)
+        code=request.data['payement_code']
+        delivery_address_id=request.data['address_id']
+        delivery_address=Address.objects.get(id=delivery_address_id)
+
+
+        new_order=Order.objects.create(
+            user=user,
+            code=code,
+            delivery_address=delivery_address,
+            copoun=cart.copoun,
+            total=cart.cart_total,
+            total_with_copoun=cart.total_with_copon,
+                        
+        )
+
+        for item in cart_detail:
+            product=Product.objects.get(id=item.product.id)
+            Order_Detail.objects.create(
+                order=new_order,
+                product=product,
+                quantity=item.quantity,
+                price=product.price,
+                total=product.price * item.quantity
+            )
+
+            product.quantity -=item.quantity
+            product.save()
+        cart.status='Completed'
+        cart.save()
+
+        return Response({'message':'ok daone create new_order'},status=status.HTTP_201_CREATED)
+
+
+class Get_Update_Delete_Cart(generics.GenericAPIView):
+    def get(self,request,*args,**kwargs):    # get or create cart
+        user=User.objects.get(username=self.kwargs['username'])
+        cart,created=Cart.objects.get_or_create(user=user,status='Inprogress')
+        data=CartSerializers(cart).data
+        return Response({'cart':data})
+
+    def add_update(self,request,*args,**kwargs):
+        # add or update to cart detatil
+
+        user=User.objects.get(username=self.kwargs['username'])
+        product=Product.objects.get(id=request.data['product_id'])
+        quantity=int(product.quantity)
+
+        cart=Cart.objects.get(user=user,status='Inprogress')
+        cart_detail,created=Cart_Detail.objects.get_or_create(cart=cart,product=product)
+
+        cart_detail.quantity=quantity
+        cart_detail.total=cart_detail.quantity * product.price
+
+        cart_detail.save()
+
+        return Response({'message':'ok done add and update product in cart_detail'},status=status.HTTP_202_ACCEPTED)
+        
+
+    def delete(self,request,*args,**kwargs):
+        # delete from cart_detail
+        user=User.objects.get(username=self.kwargs['username'])
+        cart=Cart.objects.get(user=user,status='Inprogress')
+        cart_detail=Cart_Detail.objects.filter(cart=cart)
+
+        product=Cart_Detail.objects.get(id=request.data['product_id'])
+        product.delete()
+
+        product.save()
+
+        return Response({'message':'ok delete from cart_detail'})
+       
+            
--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import order_list,checkout,add_to_cart
-from .api import OrderDetailApi,OrderListApi,Apply_Copoun
+from .api import OrderDetailApi,OrderListApi,Apply_Copoun,Create_Order,Get_Update_Delete_Cart
 
 
 urlpatterns = [
@@ -12,6 +12,11 @@ urlpatterns = [
     path('api/<str:username>/list',OrderListApi.as_view()),
     path('api/<str:username>/<int:pk>',OrderDetailApi.as_view()),
     path('api/<str:username>/apply_copoun',Apply_Copoun.as_view()),
+
+    path('api/create_order/<str:username>',Create_Order.as_view()),
+    path('api/update_delete_cart/<str:username>',Get_Update_Delete_Cart.as_view()),
+
+
 
  
   
